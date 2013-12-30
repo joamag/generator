eb4f7403c927eb2470214c017965ea132c532328
joamag <joamag@gmail.com>
2013-12-30 17:53:46 +0000

    small comments added

--- main.go
+++ main.go
@@ -15,12 +15,23 @@ const (
 )
 
 func Loader(host string) {
+	// runs the tcp connection and then sends some
+	// information to the connection requesting the
+	// main page in http "language"
 	conn, err := net.Dial("tcp", host)
 	if err != nil {
 		return
 	}
 	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+
+	// creates a new reader (buffered) from the created
+	// connection that is going to be used to read the
+	// initial line from it
 	reader := bufio.NewReader(conn)
+
+	// reads the first line from the reader (status line)
+	// and then trims it removing the newline sequence
+	// from it and then printing the information
 	status, err := reader.ReadString('\n')
 	status = strings.TrimRight(status, "\r\n")
 	fmt.Println(status)
